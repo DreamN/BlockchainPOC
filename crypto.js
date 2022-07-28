67579131c3619e8f9c461a2638f1f5068183821b
Isara Naranirattisai <isaradream@gmail.com>
2022-07-28 17:42:02 +0700

    display signed signature

--- crypto.js
+++ crypto.js
@@ -2,6 +2,11 @@ let connected = false;
 let web3 = null;    
 let account = null;
 
+let signText = document.getElementById("sign-text");
+let signButton = document.getElementById("signButton");
+let walletAddressesSelect = document.getElementById("wallet-addresses-select");
+let signatureTextArea = document.getElementById("signature-text");
+
 async function connectAccount(){
     if (window.ethereum) {
         await window.ethereum.request({method: 'eth_requestAccounts'});
@@ -18,7 +23,7 @@ async function connectAccount(){
 }
 
 function enableOperationButtons(){
-    let signButton = document.getElementById("signButton");
+    signText.disabled = false;
     signButton.disabled = false;
 }
 
@@ -29,21 +34,35 @@ function updateAccountDetails(){
     });
 }
 
+function cleanWalletAddressSelect(walletAddressesSelect){
+    while (walletAddressesSelect.options.length) {
+        walletAddressesSelect.remove(0);
+    }
+}
+
 function updateAccountAddress(addresses){
-    let walletAddressesValueText = document.getElementById("wallet-addresses-value");
-    walletAddressesValueText.innerHTML = addresses.join(",");
+    cleanWalletAddressSelect(walletAddressesSelect);
+    for(var i = 0, l = addresses.length; i < l; i++){
+        let walletAddress = addresses[i];
+        console.log(`Hi ${walletAddress}`);
+        walletAddressesSelect.options.add( new Option(walletAddress, walletAddress) );
+    }
+    if(walletAddressesSelect.options.length > 0){
+        walletAddressesSelect.options[0].selected = true;
+    }
 }
 
 function sign(){
-    let message = "test";
+    let message = signText.value;
     let from = this.accounts[0];
     var sha3Message = web3.utils.sha3(message);
-    console.log(`Sign from: ${from}`);
+    console.log(`Sign from: [${from}], message: [${message}]`);
     web3.eth.sign(sha3Message, web3.utils.toChecksumAddress(from), function (err, signature) {
         if(err){
             console.log(`Sign error: ${err.message}`);
             return;
         }
-        console.log(`Signature: ${signature}`);
+        console.log(`Signature: [${signature}]`);
+        signatureTextArea.innerHTML = signature;
     });
 }
