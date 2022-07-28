5b79c546c57a21f1cf503d7212e4bce66a55521a
Isara Naranirattisai <isaradream@gmail.com>
2022-07-28 16:58:15 +0700

    impl: Sign

--- crypto.js
+++ crypto.js
@@ -1,31 +1,49 @@
-let loaded = false;
+let connected = false;
 let web3 = null;    
+let account = null;
 
 async function connectAccount(){
     if (window.ethereum) {
-        await window.ethereum.request({method: 'eth_requestAccounts'}).then(addr => updateAccountAddress(addr));
+        await window.ethereum.request({method: 'eth_requestAccounts'});
         web3 = new Web3(window.ethereum);
-        window.web3 = web3;
-        console.log("Success!");
+        console.log("Connecting to the wallet successfully!");
+        updateAccountDetails();
     }
     else {
         console.log("There's no metamask installed on this browser");
     }
     
-    loaded = true;
+    connected = true;
     enableOperationButtons();
 }
 
 function enableOperationButtons(){
+    let signButton = document.getElementById("signButton");
+    signButton.disabled = false;
 }
 
-function updateAccountAddress(addrs){
-    let walletAddressValueText = document.getElementById("wallet-address-value");
-    walletAddressValueText.innerHTML = addrs.join(",");
+function updateAccountDetails(){
+    web3.eth.getAccounts().then(function(accounts){
+        this.accounts = accounts;
+        updateAccountAddress(accounts)
+    });
+}
+
+function updateAccountAddress(addresses){
+    let walletAddressesValueText = document.getElementById("wallet-addresses-value");
+    walletAddressesValueText.innerHTML = addresses.join(",");
 }
 
 function sign(){
-    web3.eth.sign(web3.eth.defaultAccount, web3.utils.sha3('test'), function (err, signature) {
-        console.log(signature);  // But maybe do some error checking. :-)
+    let message = "test";
+    let from = this.accounts[0];
+    var sha3Message = web3.utils.sha3(message);
+    console.log(`Sign from: ${from}`);
+    web3.eth.sign(sha3Message, web3.utils.toChecksumAddress(from), function (err, signature) {
+        if(err){
+            console.log(`Sign error: ${err.message}`);
+            return;
+        }
+        console.log(`Signature: ${signature}`);
     });
 }
